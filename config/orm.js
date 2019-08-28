1e49b069a7a4ff79f618550462427fb9fc965031
Yuka Nishijima <48897211+yukanishijima@users.noreply.github.com>
2019-08-28 16:35:12 -0400

    Add base logic

---
+++ config/orm.js
@@ -0,0 +1,33 @@
+const connection = require("../config/connection.js");
+
+const orm = {
+  selectAll: function (tableName, cb) {
+    const queryString = `SELECT * FROM ${tableName};`;
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function (tableName, colName, val, cb) {
+    const querySting = `INSERT INTO ${tableName} (${colName}) VALUES ("${val}");`;
+    connection.query(querySting, function (err, result) {
+      if (err) {
+        throw err
+      };
+      cb(result);
+    });
+  },
+  updateOne: function (tableName, colName, boolean, condition, cb) {
+    const querySting = `UPDATE ${tableName} SET ${colName} = ${boolean} WHERE ${condition};`
+    connection.query(querySting, function (err, result) {
+      if (err) {
+        throw err
+      };
+      cb(result);
+    });
+  }
+};
+
+module.exports = orm;
